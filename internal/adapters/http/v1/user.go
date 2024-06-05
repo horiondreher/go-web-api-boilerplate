9a86edc66aa38291b671c34bd85ba7214ab26179
horiondreher <horiondreher@gmail.com>
2024-06-05 00:59:52 -0300

    Changing struct

--- internal/adapters/http/v1/user.go
+++ internal/adapters/http/v1/user.go
@@ -71,8 +71,8 @@ type LoginUserRequestDto struct {
 type LoginUserResponseDto struct {
 	Email                 string    `json:"email"`
 	AccessToken           string    `json:"access_token"`
-	AccessTokenExpiresAt  time.Time `json:"access_token_expires_at"`
 	RefreshToken          string    `json:"refresh_token"`
+	AccessTokenExpiresAt  time.Time `json:"access_token_expires_at"`
 	RefreshTokenExpiresAt time.Time `json:"refresh_token_expires_at"`
 }
 
